3fa67079e944c062352acb4278d4df9927e91d69
Anush Kothuri <160388136+Anushkothuri@users.noreply.github.com>
2024-08-08 21:37:19 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,13 @@
+// Conditional statements
+class If_else
+{
+    public static void main(String args[])
+    {
+        int a=30;
+        int b=12;
+        if(a<b)
+        System.out.println("Condition is true");
+        else
+        System.out.println("Condition is false");  //Output : Condition is false
+    }
+}
