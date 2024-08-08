c0ef1e05ca5a68abfd607bd20cf48232be39dee6
Anush Kothuri <160388136+Anushkothuri@users.noreply.github.com>
2024-08-08 21:33:00 +0530

    Add files via upload

---
+++ Relational_Operators.java
@@ -0,0 +1,15 @@
+// Relational Operators
+class Relational_Operators
+{
+    public static void main(String[] args) 
+    {
+        int a=23;
+        int b=49;
+        System.out.println("a==b: "+(a==b));
+        System.out.println("a!=b: "+(a!=b));
+        System.out.println("a>b: "+(a>b));
+        System.out.println("a<b: "+(a<b));
+        System.out.println("a<=b: "+(a<=b));
+        System.out.println("a>=b: "+(a>=b));
+    }
+}
