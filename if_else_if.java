0411193033c6c6312ef67bc9ac913fca548ab95d
Anush Kothuri <160388136+Anushkothuri@users.noreply.github.com>
2024-08-08 21:28:47 +0530

    Add files via upload

---
+++ if_else_if.java
@@ -0,0 +1,15 @@
+class If_else_if
+{
+    public static void main(String args[])
+    {
+        int a=30;
+        int b=12;
+        int c=8;
+        if(a<b)
+        System.out.println("if");
+        else if(c>b)
+        System.out.println("else if"); 
+        else
+        System.out.println("else"); // Output : else
+    }
+}
